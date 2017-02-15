39826d7b099db4775795eac6156105f63d453f99
Iva Babukova <iva.babukova@skyscanner.net>
2017-02-15 20:53:40 +0000

    Selects m flights out of all feasible flights randomly

--- generator.py
+++ generator.py
@@ -1,6 +1,7 @@
 import json
 import datetime
 import pprint
+import random
 
 pp = pprint.PrettyPrinter(indent=4)
 
@@ -8,7 +9,10 @@ airports = []
 dests = []
 hpt = ""
 T = 0
-instanceFile = ""
+instanceFile = "instances/test"
+fFile = instanceFile + "_F"
+instancePropsFile = instanceFile + "_props" # saves all data required to generate instanceFile again
+m = 0
 
 # from which date do I take T to start?
 
@@ -30,7 +34,7 @@ def calculateDuration(stamp1, stamp2):
     return round(durationFrac, 2)
 
 def parseConfig():
-    global airports, dests, hpt, T, instanceFile
+    global airports, dests, hpt, T, m
     with open("instanceConfig", "r") as f:
         for line in f:
             conf = json.loads(line)
@@ -40,10 +44,16 @@ def parseConfig():
                 dests = conf[1:]
             elif conf[0] == "home point":
                 hpt = conf[1]
+            elif conf[0] == "m":
+                m = int(conf[1])
             elif conf[0] == "T":
                 T = float(conf[1])
-            elif conf[0] == "instanceName":
-                instanceFile = conf[1]
+
+def generateHC1():
+    pass
+
+def generateHC2():
+    pass
 
 def generateA():
     A = []
@@ -64,35 +74,63 @@ def generateA():
     return A
 
 def generateF():
-    global airports, dests, hpt, T
+    global airports, T, fFile
     with open("flightsCorpus", "r") as f:
         currentDate = 0
         earliestStamp = 0
         F = []
-        idnumb = 1
-        for line in f:
-            flight = json.loads(line)
-            if flight[2] in airports and flight[3] in airports:
-                if earliestStamp == 0:
-                    earliestStamp = flight[0]
-                currentDate = calculateDuration(earliestStamp, flight[0])
-                if currentDate + flight[1] <= T:
-                    flight[0] = currentDate
-                    flight.insert(0, idnumb)
-                    F.append(flight)
-                    idnumb += 1
-                else:
-                    break
+        with open(fFile, "w") as f1:
+            for line in f:
+                flight = json.loads(line)
+                if flight[2] in airports and flight[3] in airports:
+                    if earliestStamp == 0:
+                        earliestStamp = flight[0]
+                    currentDate = calculateDuration(earliestStamp, flight[0])
+                    if currentDate + flight[1] <= T:
+                        flight[0] = currentDate
+                        F.append(flight)
+                        json.dump(flight, f1)
+                        f1.write("\n")
     return F
+
+def takeFromFrandomM():
+    global F, m, instancePropsFile
+    counter = 0
+    with open(instancePropsFile, "w") as f1, open(instanceFile, "w") as f2:
+        while counter < m:
+            i = random.randint(0, len(F) - 1)
+            f1.write(str(i) + "\n")
+            f = F.pop(i)
+            f.insert(0, counter + 1)
+            json.dump(f, f2)
+            f2.write("\n")
+            counter += 1
+
 
 parseConfig()
 F = generateF()
 A = generateA()
 
 with open(instanceFile, "w") as f:
+    astats = []
+    astats.append("airports")
+    astats.append(len(A))
+    json.dump(astats, f)
+    f.write("\n")
     for airport in A:
         json.dump(airport, f)
         f.write("\n")
-    for flight in F:
-        json.dump(flight, f)
-        f.write("\n")
+    fstats = []
+    fstats.append("flights")
+    fstats.append(m)
+    # json.dump(fstats, f)
+    # f.write("\n")
+    # for flight in F:
+    #     json.dump(flight, f)
+    #     f.write("\n")
+    json.dump(["holiday"], f)
+    f.write("\n")
+    json.dump(T, f)
+    f.write("\n")
+
+takeFromFrandomM()
